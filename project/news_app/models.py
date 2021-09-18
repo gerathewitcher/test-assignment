c1c7bb6c8f91532c88e67f4ba8dd6897ef6cc368
gerathewitcher <gerandokov1337@gmail.com>
2021-09-18 17:51:08 +0300

    Created models

--- project/news_app/models.py
+++ project/news_app/models.py
@@ -2,4 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class News(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    text = models.TextField(blank=True, verbose_name='Содержание')
+    image = models.ImageField(upload_to='images/%Y-%m-%d/', verbose_name='Изображение')
+    tags = models.ManyToManyField('Tag', verbose_name='Теги')
+    created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name = 'Новости'
+        ordering = ('-created',)
+
+    def __str__(self) -> str:
+        return f'#{self.id} {self.title}' 
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Наименование тега')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+    
+    def __str__(self) -> str:
+        return self.name 
